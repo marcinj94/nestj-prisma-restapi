2df75a2109dd403aa99edee1a352f1df932a6647
Marcin Jusiak <marcin.jusiak@gmail.com>
2023-12-06 23:15:38 +0100

    feat(swagger): set up Swagger
    Swagger is a tool to document your API using the OpenAPI specification. While app is running, open
    your browser and navigate to http://localhost:4200/api - you should see the Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const chalk = require('chalk');
@@ -8,10 +9,25 @@ const port = 4200;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Median APP')
+    .setDescription(
+      'Build REST API with nestJS and Prisma | TUTORIAL: https://www.prisma.io/blog/nestjs-prisma-rest-api-7D056s1BmOL0',
+    )
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // eslint-disable-next-line no-console
   console.log(
-    chalk.green(`
->>> App is listening on ${chalk.bgGreen(`http://localhost:${port}`)}
+    chalk.cyan(`
+>>> App is listening on ${chalk.bgGreen(`http://localhost:${port}`)}`),
+  );
+  // eslint-disable-next-line no-console
+  console.log(
+    chalk.green(`>>> Swagger is listening on ${chalk.bgCyan(`http://localhost:${port}/api`)}
   `),
   );
 
