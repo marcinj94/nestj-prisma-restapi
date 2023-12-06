542d72b5458984415405437a85cf9e1aaf064dc7
Marcin Jusiak <marcin.jusiak@gmail.com>
2023-12-06 22:02:07 +0100

    feat(postgresql): create a PostgreSQL instance
    Install and run PostgreSQL on your machine through a Docker container. To run PostgreSQL database
    run the following command in main folder of your project: docker-compose up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const chalk = require('chalk');
+
+const port = 4200;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4200);
+
+  // eslint-disable-next-line no-console
+  console.log(
+    chalk.green(`
+>>> App is listening on ${chalk.bgGreen(`http://localhost:${port}`)}
+  `),
+  );
+
+  await app.listen(port);
 }
 bootstrap();
