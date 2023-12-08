6a250db5af41753b433d50ef82d6c72f36c0fd17
Marcin Jusiak <marcin.jusiak@gmail.com>
2023-12-08 16:25:24 +0100

    feat(validation): add validation rules to CreateArticleDto
    We will learn how to validate the input using NestJS Pipes. One of the advantages of using
    decorators for validation is that the CreateArticleDto remains the single source of  truth for all
    arguments to the POSt /articles endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const chalk = require('chalk');
 
@@ -8,6 +10,18 @@ const port = 4200;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Currently, for both of these endpoints it is possible to send additional properties that are not defined in the DTO.
+  // This can lead to unforeseen bugs or security issues. For example, you could manually pass invalid createdAt and updatedAt values to the POST /articles endpoint.
+  // Since TypeScript type information is not available at run-time, your application will not be able to identify that these fields are not available in the DTO.
+  // In this way, you can inject invalid values. Here you have created an article that has an updatedAt value that precedes createdAt, which does not make sense.
+  // To prevent this, you will need to filter any unnecessary fields/properties from client requests. Fortunately, NestJS provides an out-of-the-box for this as well.
+  // All you need to do is pass the whitelist: true option when initializing the ValidationPipe inside your application.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Median APP')
