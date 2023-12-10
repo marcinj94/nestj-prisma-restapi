d025032d2bc8c90ecd9313b6b6a31db2c33e58c2
Marcin Jusiak <marcin.jusiak@gmail.com>
2023-12-10 20:44:18 +0100

    feat(validation): transform dynamic URL paths with ParseIntPipe
    The ParseIntPipe will intercept the id parameter of string type and automatically parse it to number
    before passing it to the appropriate route handler. This also has the advantage of documenting the
    id parameter correctly as a number inside Swagger.

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
@@ -41,23 +41,23 @@ export class ArticlesController {
   @ApiOkResponse({
     type: ArticleEntity,
   })
-  findOne(@Param('id') id: string) {
-    return this.articlesService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.findOne(id);
   }
 
   @Patch(':id')
   @ApiOkResponse({
     type: ArticleEntity,
   })
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articlesService.update(+id, updateArticleDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(id, updateArticleDto);
   }
 
   @Delete(':id')
   @ApiOkResponse({
     type: ArticleEntity,
   })
-  remove(@Param('id') id: string) {
-    return this.articlesService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.remove(id);
   }
 }
